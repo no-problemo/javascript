f30b4ce62124a1c167793d82c1cf198fc406049a
blogapik <b1r0w0@ymail.com>
2014-12-21 21:26:45 +0700

    menampilkan data json dalam bentuk html-list

--- jsonList ( tampilkan json dalam html-list )/README.md
+++ jsonList ( tampilkan json dalam html-list )/README.md
@@ -3,9 +3,9 @@
 MENAMPILKAN DATA JSON DALAM BENTUK HTML-LIST :<br>
 
     jsonList(json,holder,callback,branchOnly);
-**json :** data json misal : var json={root-level1:{branch-level2:{branch-level3:{leaf-level4:0}}}} <br>
+**json :** data json misal : var json={root-level1:{\_:1,branch-level2:{\_:1,branch-level3:{\_:1,leaf-level4:0}}}} , _:1 property _ tidak ditampilkan karena dipakai u/ menyimpan informasi keadaan expand(1) / collapse(0) <br>
 **holder :** tempat u/ meletakkan html-list nya misal : &lt;div id="holder" >&lt;/div> <br>
 **callback :** fungsi yang dipanggil ketika item di html-list diklik misal : jsonList(json,holder,function(path){/* skrip \*/}); path berupa array , misal item leaf-level4 diklik maka isi path : [root-level1,branch-level2,branch-level3,leaf-level4] <br>
 **branchOnly :**(optional) hanya menampilkan branch nya saja / tidak menampilkan leaf nya, misal : jsonList(json,holder,function(path){/* skrip \*/},1); <br><br>
 contoh penerapan jsonList di file jsonList.htm <br>
-**[demo](http://jsfiddle.net/j33hrtpm/ "jsonList demo")**
+**[demo](http://jsfiddle.net/j33hrtpm/1/ "jsonList demo")**
--- jsonList ( tampilkan json dalam html-list )/jsonList.js
+++ jsonList ( tampilkan json dalam html-list )/jsonList.js
@@ -1,38 +1,58 @@
 function jsonList(a,b,cb,c){ 
   function rekursi(a){
+    var klik='';
     s+='<ul>';
-    for(var i in a)
-      if(c===undefined || typeof a[i]==='object'){      
-        s+='<li><span>'+i+'</span>';
+    for(var i in a){
+      if(i=='_'){if(a._==0)break;else continue}
+      klik='';
+      if(i==u[j]){j++;if(j==u.length){j=0;klik='klik '}}
+      if(c==undefined || typeof a[i]=='object'){
+        s+='<li><span class="'+klik+(typeof a[i]=='object'?'branch':'leaf')+'" >'+i+'</span>';
         rekursi(a[i]);
         s+='</li>';
       }
+    }
     s+='</ul>'
   }
   function rndr(){
-      s='';
-      rekursi(a);
-      if(s!==ss){
-        f.innerHTML=s;
-        ss=s;
-      }
-    setTimeout(rndr,200)
+    s='';
+    rekursi(a);
+    if(s!=t){
+      f.innerHTML=s;
+      t=s
+    }
+    setTimeout(rndr,300)
   }
-  var s='',ss='';
+  var s='',t='',u=[],j=0;
   var f=document.getElementById(b);
   rndr();
   f.onclick=function(e){
     e=e||event;
     var f=e.srcElement||e.target;
     if(f.tagName==='SPAN'){
-      var g=f.nextSibling.style;
-      g.display=g.display==='none'?'block':'none';
-      var s=[];
+      u=[];
       do{
-        s.push(f.innerHTML);
+        u.unshift(f.innerHTML);
         f=f.parentNode.parentNode.previousSibling
       }while(f)
-      cb(s.reverse());
+      var ptr=a;
+      for(var i in u)ptr=ptr[u[i]];
+      if(typeof ptr === 'object' && '_' in ptr)ptr._^=1;
+      cb(u,ptr);
     }
   }
+}
+
+var json={'1level1':{_:1,'1level2(leaf)':0,'2level2':{_:1,'1level3':{_:1,'1level4(leaf)':0,'2level4':{_:1,'1level5(leaf)':0},'3level4(leaf)':0}},'3level2(leaf)':0},'2level1':{_:1,'1level2(leaf)':0,'2level2':{_:1,'1level3(leaf)':0},'3level2(leaf)':0},'3level1':{_:1,'1level2(leaf)':0,'2level2':{_:1,'1level3(leaf)':0},'3level2(leaf)':0}};
+var refer={};
+jsonList(json,'holder',function(a,c){
+  path.innerHTML=a.join('>')+' :';
+  refer=c;
+})
+function addItem(z,b){
+  var a=z.innerHTML.split(': ');
+  if(typeof refer!=='object'){alert('tidak bisa tambah item di leaf');return 0}
+  refer[a[1]]=b;
+  a[1]=Math.random()+(b===0?'(leaf)':'');
+  z.innerHTML=a.join(': ')
 }
